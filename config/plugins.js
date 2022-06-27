c9084231db9b4ec972718c449a4e315bd837fd89
Carlos Toro <carlostorom.96@gmail.com>
2022-06-26 21:24:57 -0400

    images with cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: 'potosi',
+          api_key: '914769589592892',
+          api_secret: '0K_-Qk936xCEvF4IdrU52HBCvqY'
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
