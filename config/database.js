5770226bde3ea43667d47016d83207a630b55219
Carlos Toro <carlostorom.96@gmail.com>
2022-07-11 21:15:11 -0400

    prueba data base

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'ec2-34-225-159-178.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd4rj7gg80f3mns'),
+      user: env('DATABASE_USERNAME', 'aovcsosbyanert'),
+      password: env('DATABASE_PASSWORD', '9888ee156d7fa1dc94e180e2713fdfc029668a24977dde984e23bb00d5804c5f'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
