2ef4002958ec81b12cafb2dc6c203b53ed7f5c6f
Carlos Toro <carlostorom.96@gmail.com>
2022-09-20 13:15:31 -0400

    deploy: deploy to render

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'ec2-34-225-159-178.compute-1.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd4rj7gg80f3mns'),
-      user: env('DATABASE_USERNAME', 'aovcsosbyanert'),
-      password: env('DATABASE_PASSWORD', '9888ee156d7fa1dc94e180e2713fdfc029668a24977dde984e23bb00d5804c5f'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: env("DATABASE_HOST", "dpg-cckuanhgp3jhcld5sttg-a"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "dbencuentra"),
+      user: env("DATABASE_USERNAME", "carlostoro"),
+      password: env("DATABASE_PASSWORD", "DMP70EMiuWR12dlUPq9uYbhBkAlS3qg5"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
     },
     debug: false,
